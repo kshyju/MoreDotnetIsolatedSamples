7b0a02b5b5fe0e5df1282b3e482f7ee5790196e0
Shyju Krishnankutty <connectshyju@gmail.com>
2023-11-30 15:36:54 -0800

    Adding .NET 8 minimal Http trigger app.

---
+++ Net8SimpleHttpTrigger/HelloHttp.cs
@@ -0,0 +1,43 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Net8SimpleHttpTrigger
+{
+    public class HelloHttp
+    {
+        private readonly ILogger _logger;
+
+        public HelloHttp(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HelloHttp>();
+        }
+
+        [Function("HelloHttp")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Welcome to Azure Functions! HelloHttp");
+
+            return response;
+        }
+
+        [Function("HelloHttpAsync")]
+        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            await Task.Delay(100); // Simulate async work
+
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Welcome to Azure Functions! HelloHttpAsync");
+
+            return response;
+        }
+    }
+}
