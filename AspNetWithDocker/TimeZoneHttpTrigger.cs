eface7e55db1dfeed7733ee184f31241c05553a3
Shyju Krishnankutty <connectshyju@gmail.com>
2024-03-22 15:35:47 -0700

    Adding an http trigger to print TimeZoneInfo.

---
+++ AspNetWithDocker/TimeZoneHttpTrigger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using System.Runtime.InteropServices;
+
+namespace Net8SimpleHttpTrigger
+{
+    public sealed class TimeZoneHttpTrigger
+    {
+        [Function("TimeZone")]
+        public IActionResult GetTimeZone([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        {
+            return new OkObjectResult(new
+            {
+                RuntimeInformation.FrameworkDescription,
+                RuntimeInformation.OSDescription,
+                TimeZone1 = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time"),
+                TimeZone2 = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time")
+            });
+        }
+    }
+}
