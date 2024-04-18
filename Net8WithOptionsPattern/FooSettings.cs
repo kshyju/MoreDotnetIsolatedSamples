cf7ac613e48c3e99eac80537b7a01e237bbad8d0
Shyju Krishnankutty <connectshyju@gmail.com>
2024-04-18 06:30:07 -0700

    Adding sample app to demonstrate how to use OptionsPattern

---
+++ Net8WithOptionsPattern/FooSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace  MyCompany.Functions
+{
+    public sealed class FooSettings
+    {
+        public string? ApiVersion { get; set; }
+        public Uri? ApiUrl { get; set; }
+        public string[]? AllowedDomains { get; set; }
+    }
+}
---
+++ Net8WithOptionsPattern/HttpTriggerFunction.cs
@@ -0,0 +1,35 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace  MyCompany.Functions
+{
+    public sealed class HttpTriggerFunction
+    {
+        private readonly ILogger _logger;
+        private readonly FooSettings _fooSettings;
+        public HttpTriggerFunction(ILoggerFactory loggerFactory, IOptions<FooSettings> fooSettingsOption)
+        {
+            _logger = loggerFactory.CreateLogger<HttpTriggerFunction>();
+            _fooSettings = fooSettingsOption.Value;
+        }
+
+        [Function("HelloHttp")]
+        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            await Task.Delay(100); // Simulate some async work
+
+            var version = _fooSettings.ApiVersion;
+            var url = _fooSettings.ApiUrl;
+            _logger.LogInformation($"C# HTTP trigger function processed a request. version:{version}. ApiUrl:{url}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            response.WriteString("Welcome to Azure Functions! HelloHttpAsync");
+
+            return response;
+        }
+    }
+}
